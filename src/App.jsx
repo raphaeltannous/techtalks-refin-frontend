b73db5993a43746a43df189dabffeeb5c84a6a82
hussein zeidan <ahmadhusseinzeidan@gmail.com>
2026-05-13 12:39:37 +0300

    Create job applications management dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage"
-//import jobs page here
+import ApplicationsDashboard from "./pages/ApplicationsDashboard"
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
 
@@ -8,17 +8,16 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/*Unprotected routes*/}
+        {/* Unprotected routes */}
 
         <Route path="/" element={<LandingPage />} />
-        {/*Jobs page here*/}
+        <Route path="/applications-dashboard" element={<ApplicationsDashboard />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
 
-        {/*Protected Routes*/}
-        
-        {/*Dashboard page here*/}
-        
+        {/* Protected Routes */}
+
+        {/* Dashboard page here */}
       </Routes>
     </Router>
   )
